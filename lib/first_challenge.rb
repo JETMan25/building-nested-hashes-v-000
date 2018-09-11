c7cf85bc34442a7096e05cd6fcc756da44aab0dc
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-11 16:00:17 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,8 +3,8 @@ def first_challenge
      :montague => {
        :patriarch => {:name "Lord Montague", :age "53"},
        :matriach => {:name "Lady Montague", :age "54"},
-       :hero => {name: "Romeo", age "15", :status "alive"},
-       :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+       :hero => {:name "Romeo", :age "15", :status "alive"},
+       :hero_friends => [{:name "Benvolio", :age "17", :attitude "worried"}, {:name "Mercutio", :age "18", :attitude "hot-headed"}]
      },
      :capulet => {
        :patriarch => {name: "Lord Capulet", age: "50"},
